0dc958e7e0f2d98297d0b319fef357852837f61d
Mariah Crayfish <63157435+MariahCrayfish@users.noreply.github.com>
2026-06-07 13:41:13 +0100

    Update kinkAutomation.js

--- kinkAutomation.js
+++ kinkAutomation.js
@@ -1,64 +1,86 @@
-function kinkSelect(category){
-  kinks = document.querySelector('#kinks')
-  kl = kinks.length
-  for(let i=0; i<kl;i++){
-    if(kinks[i].classList==category){
-      kinks[i].selected = true
+function kinkSelect(category) {
+  const kinks = document.querySelector("#kinks");
+  if (!kinks) return;
+  const options = kinks.options || kinks;
+  const kl = options.length;
+  for (let i = 0; i < kl; i++) {
+    const opt = options[i];
+    if (opt && opt.classList && opt.classList.contains(category)) {
+      opt.selected = true;
     }
   }
 }
 
-function ckinkSelect(text){
-  ck = document.querySelector('#customKinks')
-  ckl = ck.length
-  for(let i=0;i<ckl;i++){
-    if(ck[i].innerText == text){
-      document.querySelector('#customKinks')[i].selected = true
+function ckinkSelect(text) {
+  const ck = document.querySelector("#customKinks");
+  if (!ck) return;
+  const options = ck.options || ck;
+  const ckl = options.length;
+  for (let i = 0; i < ckl; i++) {
+    const opt = options[i];
+    if (opt && opt.innerText == text) {
+      opt.selected = true;
     }
   }
 }
 
-function kinksPlacer(categories, kinkText){
-  for(ii=0;ii<4;ii++){
-    kinkSelect(categories[ii])
-    ckinkSelect(kinkText[ii])
-    document.querySelector('#addKink').click()
+function kinksPlacer(categories, kinkText) {
+  for (let ii = 0; ii < 4; ii++) {
+    kinkSelect(categories[ii]);
+    ckinkSelect(kinkText[ii]);
+    const addBtn = document.querySelector("#addKink");
+    if (addBtn) addBtn.click();
   }
 }
 
-function textPicker(){
-  faveText = document.querySelector("#fave").value
-  yesText = document.querySelector("#yes").value
-  maybeText = document.querySelector("#maybe").value
-  noText = document.querySelector("#no").value
-  return [faveText, yesText, maybeText, noText]
+function textPicker() {
+  faveText = document.querySelector("#fave").value;
+  yesText = document.querySelector("#yes").value;
+  maybeText = document.querySelector("#maybe").value;
+  noText = document.querySelector("#no").value;
+  return [faveText, yesText, maybeText, noText];
 }
 
-function organiseKinks(){
-  categories = ["fave", "yes", "maybe", "no"]
-  kinkText = textPicker()
-  groupCount = document.querySelector("#groups").children[1].childElementCount
+function organiseKinks() {
+  const categories = ["fave", "yes", "maybe", "no"];
+  const kinkText = textPicker();
+  const groupsEl = document.querySelector("#groups");
+  if (!groupsEl) return;
+  const listContainer = groupsEl.children[1];
+  const groupCount = listContainer ? listContainer.childElementCount : 0;
 
-  for(j=0;j<groupCount;j++){
-    document.querySelector('#groups').children[1][j].selected = true;
+  for (let j = 0; j < groupCount; j++) {
+    listContainer.children[j].selected = true;
     FList.Subfetish.Ui.populateKinks();
-    FList.Subfetish.Ui.Lists.Cache.recache('groups');
-    kinksPlacer(categories, kinkText)
-	}
+    FList.Subfetish.Ui.Lists.Cache.recache("groups");
+    kinksPlacer(categories, kinkText);
+  }
 }
 
-document.getElementById('description').insertAdjacentHTML("beforebegin", "<div id='kinkAutomationLabels'>")
+const descEl = document.getElementById("description");
+if (descEl)
+  descEl.insertAdjacentHTML(
+    "beforebegin",
+    "<div id='kinkAutomationLabels'></div>",
+  );
 
-document.querySelector('#kinkAutomationLabels').innerHTML = `<h3>Custom Kink Names</h3>\
-<table>\
-<form>\
-<tr><td><label for='fave'>Fave: </label>    </td><td><input type='text' id='fave' name='fave'></input></td></tr>\
-<tr><td><label for='yes'>Yes: </label>      </td><td><input type='text' id='yes' name='yes'></input></td></tr>\
-<tr><td><label for='maybe'>Maybe: </label>  </td><td><input type='text' id='maybe' name='maybe'></input></td></tr>\
-<tr><td><label for='no'>No: </label>        </td><td><input type='text' id='no' name='no'></input></td></tr>\
-<tr><td><button id="organiseKinks"'>Click here to organise kinks</button></td></tr>\
-</form></table>`;
+const labelsEl = document.querySelector("#kinkAutomationLabels");
+if (labelsEl) {
+  labelsEl.innerHTML = `<h3>Custom Kink Names</h3>
+  <table>
+    <form>
+      <tr><td><label for='fave'>Fave: </label></td><td><input type='text' id='fave' name='fave'></td></tr>
+      <tr><td><label for='yes'>Yes: </label></td><td><input type='text' id='yes' name='yes'></td></tr>
+      <tr><td><label for='maybe'>Maybe: </label></td><td><input type='text' id='maybe' name='maybe'></td></tr>
+      <tr><td><label for='no'>No: </label></td><td><input type='text' id='no' name='no'></td></tr>
+      <tr><td><button id="organiseKinks">Click here to organise kinks</button></td></tr>
+    </form>
+  </table>`;
 
-document.querySelector("#organiseKinks").addEventListener('click', function(e){
-  return organiseKinks();
-})
+  const organiseBtn = document.querySelector("#organiseKinks");
+  if (organiseBtn)
+    organiseBtn.addEventListener("click", function (e) {
+      e.preventDefault();
+      organiseKinks();
+    });
+}
